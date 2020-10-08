ba65680e6ff380d25830cfd0cb981cfc09ca17e6
noconsulate <noconsulate@gmail.com>
2020-10-08 14:25:13 -0600

    some view

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>trustless-freelancer</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
 </script>
 
 <style>
@@ -21,8 +13,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 30px;
 }
 </style>
---
+++ interface/src/services/web3.js
@@ -0,0 +1,4 @@
+import Web3 from "web3";
+import Freelancer from "../../../contract/build";
+
+S
