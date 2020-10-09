1665d97f0311ace2733da6c27e68a206fe6bf880
noconsulate <noconsulate@gmail.com>
2020-10-09 11:49:42 -0600

    setupLoader sux, switching to web3.eth.Contract

--- contract/init.js
+++ contract/init.js
@@ -2,7 +2,12 @@ const Web3 = require('web3');
 const { setupLoader } = require('@openzeppelin/contract-loader');
 
 const web3 = new Web3('http://localhost:8545');
-const loader = setupLoader({ provider: web3 }).web3;
+// const loader = setupLoader({ 
+//   provider: web3,
+//   defaultGas: 50000,
+//   defaultGasPrice: 1e6,
+// }).web3;
+const loader = setupLoader({provider: web3}).web3;
 
 const address = '0x797b99ac73F6b5D980fc0685756Cf0e1Bfe564C0';
 const freelancer = loader.fromArtifact('Freelancer', address)
@@ -12,17 +17,19 @@ async function main() {
   const accounts = await web3.eth.getAccounts();
   console.log(accounts);
 
-  await freelancer.methods.setMerchant('0xb8dC500be5cfEc13E4255aa6334A7051f47C6474').send({
-    from: accounts[0],
-    gas: 50000,
-    gasPrice: 1e6,
+  // fund contract and set client
+  web3.eth.sendTransaction({
+    from: accounts[1],
+    to: address,
+    value: web3.utils.toWei('.1', 'ether'),
+  });
+
+  // Set merchant 
+  await freelancer.methods.setMerchant('0x83316a84d99c1232d0A596AA95c7dd1F488a3952').send({
+    from: accounts[0], gas: 5000000, gasPrice: 1e6,
   })
 
-  await freelancer.methods.clientMarkReceived().send({
-    from: accounts[1],
-    gas: 50000,
-    gasPrice: 1e6, 
-  });
+
 
 }
 
