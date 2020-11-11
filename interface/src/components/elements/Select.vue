36365aa2996aade7c10844faeb96cfd5745b3e3c
noconsulate <noconsulate@gmail.com>
2020-11-11 12:46:10 -0700

    spacing

--- interface/src/components/elements/Select.vue
+++ interface/src/components/elements/Select.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="space-x-1">
     <div class="inline-block relative w-2/3">
       <select
         class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-1 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline text-sm"
