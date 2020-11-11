178524d5a0c3dceaf4bdd77a7bd73dff4014647c
noconsulate <noconsulate@gmail.com>
2020-11-11 10:40:33 -0700

    inline clients

--- interface/src/pages/MerchantView.vue
+++ interface/src/pages/MerchantView.vue
@@ -52,12 +52,45 @@
               class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
             >
               <dt class="text-sm leading-5 font-medium text-gray-500">
-                Client Address
+                Clients
               </dt>
               <dd
                 class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
               >
-                {{ escrowValues.address }}
+                <div class="inline-block relative w-2/3">
+                  <select
+                    class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-1 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline text-sm"
+                    v-model="selectedClient"
+                  >
+                    <option class="text-sm" disabled value=""
+                      >select a client</option
+                    >
+                    <option
+                      class="text-sm"
+                      v-for="client in clients"
+                      v-bind:selectedClient="client.address"
+                      v-bind:key="client.id"
+                    >
+                      {{ client.address }}
+                    </option>
+                  </select>
+                  <div
+                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+                  >
+                    <svg
+                      class="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <button @click="callGetEscrowValues" class="btn flex-1">
+                  get escrow
+                </button>
               </dd>
             </div>
             <div
@@ -138,11 +171,25 @@ export default {
     contractValues() {
       return this.$store.state.contractValues;
     },
+    clients() {
+      return this.$store.state.clients;
+    },
   },
   data() {
-    return {};
+    return {
+      selectedClient: null,
+    };
   },
-  methods: {},
+  methods: {
+    async callGetEscrowValues() {
+      console.log("getvalues");
+      const argsObj = {
+        client: this.selectedClient,
+        contract: this.activeContract,
+      };
+      this.$store.dispatch("fetchEscrowValues", this.selectedClient);
+    },
+  },
   created: async function() {
     if (this.activeContract == 0 || this.activeContract == null) {
       alert("please deploy a new contract or load an existing one");
