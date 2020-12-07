7f559054cb2597e76ccd58a09d835cb1fe11f5a8
noconsulate <noconsulate@gmail.com>
2020-12-07 11:25:38 -0700

    fixed for compile

--- contract/contracts/ServiceFeeReceiver.sol
+++ contract/contracts/ServiceFeeReceiver.sol
@@ -1,28 +1,27 @@
 // to distribute service fees to proper parties
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 contract ServiceFeeReceiver {
-  erc20 private token = address(0xDdbfd4Bb2CFFfe0BEe18C5F11eDc22eFe6237266);
+    ERC20 private token = ERC20(0xDdbfd4Bb2CFFfe0BEe18C5F11eDc22eFe6237266);
 
-  // receiving parties given in percentage * 10
-  address[] parties;
-  mapping(address => uint256) stakes;
-  
-  constructor() {
-    parties.push(address(0x935A3dE3217D9BB58C24343600f655141d118aeB));
-    stakes[address(0x935A3dE3217D9BB58C24343600f655141d118aeB)] = 10;
-  }
+    // receiving parties given in percentage * 10
+    address[] parties;
+    mapping(address => uint256) stakes;
 
-  function payout() public returns (bool) {
-    uint256 balance = token.balanceOf(this);
-    uint256 cut0 = balance * ( stakes(parties[0]) / 10 );
+    constructor() public {
+        parties.push(address(0x935A3dE3217D9BB58C24343600f655141d118aeB));
+        stakes[address(0x935A3dE3217D9BB58C24343600f655141d118aeB)] = 10;
+    }
 
-    bool sent = token.transfer(parties[0], cut0);
+    function payout() public returns (bool) {
+        uint256 balance = token.balanceOf(address(this));
+        uint256 cut0 = balance * (stakes[parties[0]] / 10);
 
-    require(sent, 'transfer failed');
-    return bool;
-  }
+        bool sent = token.transfer(parties[0], cut0);
 
-  function acceptPayment()
+        require(sent, "transfer failed");
+        return sent;
+    }
+
+    function acceptPayment() public {}
 }
