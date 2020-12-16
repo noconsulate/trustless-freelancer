cceb858a2d96a83ddb9f3e70ec84b61f88f9c8b6
noconsulate <noconsulate@gmail.com>
2020-12-16 12:18:52 -0700

    navbar simplified

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,50 +1,23 @@
 <template>
   <div class="container md mx-auto">
     <div class=" border border-1 h-screen flex flex-col space-y-2">
-      <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-2">
-        <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <span class="font-semibold text-xl tracking-tight"
-            >Trustless-Freelancer</span
-          >
-        </div>
-
-        <div
-          class="w-full block flex-grow lg:flex lg:items-center lg:w-auto z-40"
-        >
-          <div class="text-sm lg:flex-grow">
-            <router-link
-              :disabled="noContract"
-              to="/merchant"
-              :class="navButton"
-            >
-              Merchant
-            </router-link>
-            <router-link to="/client" :class="navButton">
-              Client
-            </router-link>
-            <router-link :disabled="noContract" to="/logs" :class="navButton">
-              Logs
-            </router-link>
-            <router-link
-              :disabled="noContract"
-              to="/contract"
-              :class="navButton"
-            >
-              Contract
-            </router-link>
-            <router-link to="/create_invoice" :class="navButton">
-              Create Invoice
-            </router-link>
-          </div>
-          <div>
-            <a
-              href="#"
-              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-              >thisdoesnothing!</a
-            >
-          </div>
-        </div>
-      </nav>
+      <div class="text-sm flex flex-row">
+        <router-link to="/merchant" :class="navButton">
+          Merchant
+        </router-link>
+        <router-link to="/client" :class="navButton">
+          Client
+        </router-link>
+        <router-link to="/logs" :class="navButton">
+          Logs
+        </router-link>
+        <router-link to="/contract" :class="navButton">
+          Contract
+        </router-link>
+        <router-link to="/create_invoice" :class="navButton">
+          Create Invoice
+        </router-link>
+      </div>
 
       <div class="flex-grow overflow-y-auto">
         <router-view></router-view>
@@ -82,8 +55,7 @@ export default {
   data() {
     return {
       routeName: this.$route.name,
-      navButton:
-        "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4",
+      navButton: "text-black px-1",
     };
   },
   watch: {
